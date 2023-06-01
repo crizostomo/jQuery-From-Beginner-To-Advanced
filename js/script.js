dfe61db1dc0bdd1f0f449410ca5ad455a1b09814
crizostomo <diogo_crizostomo@hotmail.com>
2023-06-01 17:27:42 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(function() {
+  // jQuery goes here...
+
+  // Uncomment this line to fade out the red box on page load
+   $(".red-box").fadeOut(2000);
+   $(".green-box").fadeOut(3000);
+   $(".blue-box").fadeOut(4000);
+});
