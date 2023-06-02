00de68712d81867228027952268c383b5a99db61
crizostomo <diogo_crizostomo@hotmail.com>
2023-06-02 10:56:52 -0300

    Sliding Elements Up and Down and Moving Elements

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
 $(function() {
-  // jQuery goes here...
-
-  // Uncomment this line to fade out the red box on page load
+  /**
+   // Sliding Elements Up and Down
    $(".red-box").fadeOut(2000);
-   $(".green-box").fadeOut(3000);
-   $(".blue-box").fadeOut(4000);
+   $(".blue-box").slideUp(2000);
+   $(".green-box").slideToggle(2000);
+  // $(".green-box").fadeOut(3000);
+  // $(".blue-box").fadeOut(4000);
+  $("p").hide();
+  $("p").slideDown(1000);
+   */
+
+  // Moving Elements
+  $(".blue-box").animate({
+    "margin-left": "+=200px"
+  }, 1000, "linear");
 });
