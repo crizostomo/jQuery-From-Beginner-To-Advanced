7a4e92cd40d3acbb92a97099dacdbf8cb6918ec1
crizostomo <diogo_crizostomo@hotmail.com>
2023-06-02 11:50:10 -0300

    Timing Animations Using Callback Functions

--- js/script.js
+++ js/script.js
@@ -32,8 +32,15 @@ $(function() {
   }, 1000);
   */
 
+  /**
   // Custom Animations
   $(".red-box").fadeTo(1000, 0.2);
   $(".green-box").delay(1000).fadeTo(1000, 0.5);
   $(".blue-box").delay(2000).fadeTo(1000, 0.8);
+  */
+
+  // Timing Animations Using Callback Functions
+  $(".red-box").fadeTo(1000, 0.2, function() {
+    alert("This is the callback: Animation finished");
+  });
 });
