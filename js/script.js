6fdf38d55c5585330482bf6d3125e2048524a4f4
crizostomo <diogo_crizostomo@hotmail.com>
2023-06-03 21:25:45 -0300

    Coding Activity: Creating an Image Slideshow

--- js/script.js
+++ js/script.js
@@ -135,6 +135,7 @@ $(function() {
   $(".red-box, .blue-box, .green-box").empty();
   */
 
+  /**
   // Manipulating Attributes and Properties
   // attr(), prop(), val()
   //var specialLink = $("#special-link");
@@ -152,4 +153,24 @@ $(function() {
 
   var rangeInput = $("input[type'range']");
   console.log(rangeInput.val());
+  */
+
+  // Coding Activity: Creating an Image Slideshow ********CHALLENGE********
+  var galleryImage = $(".gallery").find("img").first();
+  var images = [
+    "images/laptop-mobile_small.jpg",
+    "images/laptop-on-table_small.jpg",
+    "images/people-office-group-team_small.jpg",
+  ];
+
+  var i = 0;
+  setInterval(function() {
+    i = (i + 1) % images.length; // 0,1,2,0,1,2...
+    galleryImage.fadeOut(function() {
+      $(this).attr("src", images[i]);
+      $(this).fadeIn();
+    });
+    console.log(galleryImage.attr("src"));
+  }, 2000);
+
 });
