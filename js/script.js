ea09fd490843e0945bb0319190d177986feae17b
crizostomo <diogo_crizostomo@hotmail.com>
2023-06-05 10:51:09 -0300

    Coding Activity: Complete Form Validation on Submit

--- js/script.js
+++ js/script.js
@@ -435,6 +435,7 @@ $(function() {
   })
   */
 
+  /**
   // Handling the Submit Event
   $("#form").submit(function(event) {
     var textarea = $("#message");
@@ -445,5 +446,67 @@ $(function() {
 
     }
   });
+  */
+
+  // Coding Activity: Complete Form Validation on Submit
+  $("#form").submit(function() {
+    var name = $("#name").val();
+    var password = $("#password").val();
+    var message = $("#message").val();
+    var checked = $("#checked").is(":checked");
+
+    validateNameField(name, event);
+    validatePasswordField(password, event);
+    validateMessageField(message, event);
+    validateCheckBoxField(checked, event);
+  });
 
 });
+
+function validateNameField(name, event) {
+  if (!isValidName(name)) {
+    $("#name-feedback").text("Please enter at least two caracters");
+    event.preventDefault(); 
+  } else {
+    $("#name-feedback").text("");
+  }
+}
+
+function validatePasswordField(password, event) {
+  if (!isValidPassword(password)) {
+    $("#password-feedback").text("The password should have at least six caracters and contain a number");
+    event.preventDefault(); 
+  } else {
+    $("#password-feedback").text("");
+  }
+}
+
+function validateMessageField(message, event) {
+  if (!isValidMessage(message)) {
+    $("#message-feedback").text("Please enter a valid message");
+    event.preventDefault(); 
+  } else {
+    $("#message-feedback").text("");
+  }
+}
+
+function validateCheckBoxField(isChecked, event) {
+  if (!isChecked) {
+    $("#checkbox-feedback").text("Please, you need to read and agree with this");
+    event.preventDefault(); 
+  } else {
+    $("#checkbox-feedback").text("");
+  }
+}
+
+function isValidName(name) {
+  return name.length >= 2;
+}
+
+function isValidPassword(password) {
+  return password.length >= 6 && /.*[0-9].*/.test(password);
+}
+
+function isValidMessage(message) {
+  return message.trim() != "";
+}
